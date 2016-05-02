17538119f13ae5b7cef7af6b3e0c5a6070f8e8d5
ericksho-ipre <ericksho@gmail.com>
2016-05-02 17:55:26 -0300

    trabajo con la recuperacion de contraseña, actualmente falta onetoone restorer con user y de ahi en adelante, push del dia

--- sigi/src/BackendBundle/Controller/SecurityController.php
+++ sigi/src/BackendBundle/Controller/SecurityController.php
@@ -30,6 +30,48 @@ class SecurityController extends Controller
 	        )
 	    );
 	}
+
+	/**
+     * @Route("/lostPassword", name="lost_password_route")
+     */
+    public function lostPasswordAction(Request $request)
+	{
+		$em = $this->getDoctrine()->getManager();
+
+	    $reset = false;
+	    if($request->request->get('reset') == "true")
+	    	$reset = true;
+
+	    if($reset)
+	    {
+	    	$user = $em->getRepository('BackendBundle:User')->findOneByEmail($request->request->get('email'));
+
+	    	if(!is_null($user))
+	    	{
+	    		$random = base64_encode($user->getEmail()."///".random_bytes(10));
+
+	    		//guardar en la base de datos
+
+	    		$message = \Swift_Message::newInstance()
+					->setSubject('Recuperación de contraseña')
+					->setFrom('gestionIPre@ing.puc.cl')
+					->setTo(array($user->getEmail()))
+					->setBody("Hola, perdiste tu contraseña, el token de autenticidad es".$random.", pavo (no responda este email)")
+				;
+				$this->get('mailer')->send($message);
+	    	}
+	    }
+
+
+	    return $this->render(
+	        'security/lostPassword.html.twig',	
+	        array(
+	            // last username entered by the user
+	            'reset' => $reset,
+	        )
+	    );
+	}
+
     /**
      * @Route("/login_check", name="login_check")
      */
---
+++ sigi/src/BackendBundle/Entity/Restorer.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace BackendBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Restorer
+ *
+ * @ORM\Table(name="restorer")
+ * @ORM\Entity(repositoryClass="BackendBundle\Repository\RestorerRepository")
+ */
+class Restorer
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="time", type="datetime")
+     */
+    private $time;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="auth", type="string", length=60)
+     */
+    private $auth;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set time
+     *
+     * @param \DateTime $time
+     *
+     * @return Restorer
+     */
+    public function setTime($time)
+    {
+        $this->time = $time;
+
+        return $this;
+    }
+
+    /**
+     * Get time
+     *
+     * @return \DateTime
+     */
+    public function getTime()
+    {
+        return $this->time;
+    }
+
+    /**
+     * Set auth
+     *
+     * @param string $auth
+     *
+     * @return Restorer
+     */
+    public function setAuth($auth)
+    {
+        $this->auth = $auth;
+
+        return $this;
+    }
+
+    /**
+     * Get auth
+     *
+     * @return string
+     */
+    public function getAuth()
+    {
+        return $this->auth;
+    }
+}
+
